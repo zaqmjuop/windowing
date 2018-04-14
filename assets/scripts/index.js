980dbdf01f365a1fd521292e7a9f44dfdd6e0964
zaqmjuop <1060007045@qq.com>
2018-04-14 22:31:45 +0800

    重写

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,112 +1,142 @@
 'use strict';
+if(!document.querySelector) return false
+import isUri from "is-url"
 
 window.i = 0 //计数器
 
-var inputBox = function(option){
-  var group = $("<div></div>").addClass("input-group")
-  var input = $("<input></input>")
-    .attr("placeholder", "输入网址")
-    .attr("type", "text")
-    .addClass("form-control")
-    .attr("value", "https://www.baidu.com")
-    .attr("onkeypress", "if(event.keyCode===13){change(" + option.id +")}")
-  var holder = $("<div></div>")
-    .addClass("input-group-append")
-  var button = $("<button></button>")
-    .attr("type", "button")
-    .addClass("btn btn-outline-secondary")
-    .text("GO")
-    .attr("onclick", "change("+option.id+")")
-
-  group.append(input).append(holder.append(button))
-  return group
-}
-
-
-
-var iframe = function(option){
-  var iframe = $("<iframe></iframe>")
+const newPage = (option) => {
+  let page = $("<iframe></iframe>")
     .attr("height", window.outerHeight)
     .attr("src", (option.src || ""))
-  return iframe
+  return page
 }
 
-window.change = function(id){
-  var entrance = $("#"+id)
-  var url = entrance.find("input[type=text]").val()
-  if (entrance){
-    var option = {
-      src: url
-    }
-    var inner = iframe(option)
-    $(entrance).html('').append(inner)
+const replaceElement = (oldElement, newElement) => {
+  if(!(oldElement && newElement)) return false
+  let parent = oldElement.parentElement
+  if(!parent) return false
+  $(newElement).insertBefore($(oldElement))
+}
+
+const supportIframeHeight = () => {
+  resetIframeHeight()
+  window.onresize = ()=>{
+    resetIframeHeight()
   }
 }
 
-function dragin (event){
-  $('body').css('cursor', "w-resize")
-  //开始拖拽 因为目前只有2个框体切拖动图标在右侧 所以只要改变右侧框体的clsss col-? 即可
-  //给鼠标在body上移动时添加动作
-  var x = event.pageX
-  //改变的是右侧框体col-n 且范围在[2..10]之间
-  var col = 12 - Math.round(x/$('body').width()*12)
-  if(col>1 && col<11){
-    //拿到现存col-n 再算新的col-n 如果不一样就替换
-    var rightScr = $('.scr')[1]
-    var oldCol = rightScr.className.match(/col(-\d+)?/)[0]
-    var newCol = "col-"+col
-    if(oldCol != newCol){
-      $(rightScr).removeClass(oldCol).addClass(newCol)
-    }
+window.resetIframeHeight = () => {
+  $("iframe").each((index, element) => {
+    if (!element) return
+    let height = $(element).attr("height") || ''
+    let perHeight = height.match(/\d+%$/)
+    if (!perHeight) return
+    let per = parseInt(perHeight)
+    height = $(element).parent().height() * per / 100
+    $(element).css("height", height+"px")
+  })
+}
+
+const formatURL = (val) => {
+  if ((!val) || (typeof val != "string") ) return false
+  let hasPrefix = val.match(/^http[s]?:[/]{2}/)
+  if(!hasPrefix){
+    val = "http://" + val
   }
+  return val
+}
+
+
+const validateURL = (val) => {
+  if (!isUri) throw "isUri 未能成功加载"
+  return (isUri(val) ? val : undefined)
+}
+
+const sendFeedback = (element, validbool, feedback) => {
+  if ((!element) || (element.nodeType != 1)) return false
+  if ((!feedback) || (typeof feedback != "string")) return false
+  let bool = Boolean(validbool)
+  let inputClass = (bool) ? "is-valid" : "is-invalid"
+  let feedbackClass = (bool) ? "valid-tooltip" : "invalid-tooltip"
+  let place = $(element).parent().find(".feedback")[0]
+  if (!place) {
+    place = $("<div></div>").addClass("feedback").insertAfter(element)
+  }
+  $(element).removeClass("is-valid is-invalid").addClass(inputClass)
+  $(place).text(feedback).removeClass("valid-tooltip invalid-tooltip").addClass(feedbackClass)
+}
+
+const validFeedback = (element, feedback) => {
+  sendFeedback(element, true, feedback)
+}
+
+const invalidFeedback = (element, feedback) => {
+  sendFeedback(element, false, feedback)
+}
+
+const entranceSumit = (entrance) => {
+  if (!(entrance && $(entrance).hasClass("entrance"))) return false
+  let input = $(entrance).find("input[type=text]")[0]
+  if (!input) return false 
+  let val = $(input).val()
+  if (!val) {
+    invalidFeedback(input, "请输入网址或搜索关键词")
+    return 
+  }
+  let url = validateURL(formatURL(val)) || ("https://www.baidu.com/s?ie=UTF-8&wd=" + val)
+
+  let page = $("<iframe></iframe>")
+    .attr("src", url)
+    .attr("onload", "resetIframeHeight()")
+    .attr("width", "100%")
+    .attr("height", "100%")
+    .addClass("page")
+  $(entrance).html("").removeClass("entrance").addClass("stage").html(page)
 }
 
 
 
 
-$(document).ready(function () {
-  $(".entrance").each(function(index, element){
+const bindEntranceInput = (entrance) => {
+  if (!(entrance && $(entrance).hasClass("entrance"))) return false
+  let input = $(entrance).find("input[type=text]")[0]
+  if (!input) return
+  $(input).on("keypress", (event) => {
+    if (event.keyCode == 13) {
+      entrance.submit()
+    }
+  })
+}
+
+
+const bindEntranceSubmit = (entrance) => {
+  if (!(entrance && $(entrance).hasClass("entrance"))) return false
+  entrance.submit = () => {
+    entranceSumit(entrance)
+  }
+
+  let button = $(entrance).find("button")[0]
+  if (!button) return
+  //提交按钮
+  $(button).on("click", () => { entrance.submit() })
+}
+
+
+
+const bindEntrance = () => {
+  $(".entrance").each((index, entrance)=>{
+    if(!entrance) return
     window.i+=1
-    var id = window.i
-    $(this)
-      .attr("id", id)
-      .html(inputBox({id: id}))
-  })
-
-  // 拖拽窗体
-  $(".scr").each(function(index, element){
-    if(index>0){
-      var drager = $(element).find(".drager")
-      drager.mousedown(function(event){
-        $('body').on('mousemove', drager, dragin)
-      })
-
-      //移除监听
-      $('body').mouseleave(function (){
-        $('body').css('cursor', "default")
-        $("body").off('mousemove',dragin)
-      })
-      $('body').mouseup(function () {
-        $('body').css('cursor', "default")
-        $("body").off('mousemove', dragin)        
-      })
-
-    }
-  })
+    $(entrance).attr("id", "entrance-"+window.i)
+    bindEntranceSubmit(entrance)
+    bindEntranceInput(entrance)
+  })  
+}
 
 
-  $("iframe").each(function () {
-    this.height = window.outerHeight
-  })
 
-  //屏幕右上角显示鼠标相对于#app坐标
-  $("#app").mousemove(function(event){
-    var x = event.pageX
-    var y = event.pageY
-    var per = Math.round(x/$("body").width()*12)
-    $("#pointer").text(`X:${x}, Y:${y}, per:${per} `)
-  })
-  
-
-  
+$(document).ready(() => {
+  if (!document.querySelector) return false
+  supportIframeHeight()
+  bindEntrance()
 })
