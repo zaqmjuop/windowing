06acd2af2621b546483b8684dd5b87b0516dd6af
zaqmjuop <1060007045@qq.com>
2018-04-17 11:50:23 +0800

    基本结构

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -4,10 +4,22 @@ import isUri from "is-url"
 window.i = 0 //计数器
 
 const newPage = (option) => {
+  let loadingIcon = $("<i></i>").addClass("fa fa-spinner fa-pulse loading fa-2x") 
+  let loading = $("<div></div>").addClass("absolute-center").append(loadingIcon)//加载图标
+
   let page = $("<iframe></iframe>")
-    .attr("height", window.outerHeight)
-    .attr("src", (option.src || ""))
-  return page
+    .attr("src", (option.url || ""))
+    .attr("onload", "initIframe(this)")
+    .attr("width", "100%")
+    .attr("height", "100%")
+    .addClass("page")
+    .addClass("hide")
+
+  let holder = $("<div></div>").addClass("iframe-holder")
+
+  holder.append(loading).append(page)
+
+  return holder
 }
 
 const replaceElement = (oldElement, newElement) => {
@@ -23,6 +35,19 @@ const supportIframeHeight = () => {
     resetIframeHeight()
   }
 }
+
+window.initIframe = (iframe) => {
+  if(!iframe) return false
+  let parent = $(iframe).parent()[0]
+  $(parent).children().each((index, element)=>{
+    if (element.tagName != "IFRAME"){
+      parent.removeChild(element)
+    }
+  })
+  $(iframe).removeClass("hide")
+  window.resetIframeHeight()
+}
+
 
 window.resetIframeHeight = () => {
   $("iframe").each((index, element) => {
@@ -76,21 +101,11 @@ const invalidFeedback = (element, feedback) => {
 const entranceSumit = (entrance) => {
   if (!(entrance && $(entrance).hasClass("entrance"))) {return false}
   let input = $(entrance).find("input[type=text]")[0]
-  if (!input) {return false} 
+  if ((!input)  || (!input.value)) {return false} 
   let val = $(input).val()
-  if (!val) {
-    invalidFeedback(input, "请输入网址或搜索关键词")
-    return 
-  }
   let url = validateURL(formatURL(val)) || ("https://www.baidu.com/s?ie=UTF-8&wd=" + val)
-
-  let page = $("<iframe></iframe>")
-    .attr("src", url)
-    .attr("onload", "resetIframeHeight()")
-    .attr("width", "100%")
-    .attr("height", "100%")
-    .addClass("page")
-  $(entrance).html("").removeClass("entrance").addClass("stage").html(page)
+  let page = newPage({url})
+  $(entrance).html(page)
 }
 
 
@@ -144,3 +159,11 @@ $(document).ready(() => {
 // 前进后退 历史记录
 // 提供一个输入框随时修改网址
 // 把验证单独拿出来
+
+
+//加载iframe 
+//先放一个加载中icon√。 body添加一个iframe display=none onload="show" settimeout每秒检查一次状态 超过10秒不行就提示失败
+//因为同源问题 不能删除iframe内的a标签的target
+//解决方法
+//或者是server render
+//或者是jq ajax jsonp get (选择)
