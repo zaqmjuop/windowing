ae65c5229d1edcefc4f8527f25f60c9f59725135
zaqmjuop <1060007045@qq.com>
2018-05-03 03:13:38 +0800

    添加主页和刷新键功能

--- assets/scripts/embedded.js
+++ assets/scripts/embedded.js
@@ -1,6 +1,27 @@
 
 import initEmbeddedBody from './embeddedBody';
+import initEmbeddedHeader from './embeddedHeader';
 
+const isEmbedded = (embedded) => {
+  if (!embedded) throw new ReferenceError(`参数不能为 ${embedded}`);
+  if (embedded.nodeType !== 1 || embedded.tagName !== 'DIV') throw new ReferenceError(`${embedded} 不是一个<div>元素`);
+  if (!$(embedded).hasClass('embedded-control')) throw new ReferenceError(`${embedded} 缺少class="embedded-control"标记`);
+  return true;
+};
+
+const hasEmbeddedHeader = (embedded) => {
+  isEmbedded(embedded);
+  const embeddedHeader = $(embedded).find('card-header');
+  if (!embeddedHeader) throw new Error(`${embedded} 不包含<div class="card-header">`);
+  return true;
+};
+
+const hasEmbeddedBody = (embedded) => {
+  isEmbedded(embedded);
+  const embeddedBody = $(embedded).find('card-body');
+  if (!embeddedBody) throw new Error(`${embedded} 不包含<div class="card-body">`);
+  return true;
+};
 
 const bindEmbeddeds = () => {
   const embeddeds = $('.embedded-control');
@@ -8,8 +29,11 @@ const bindEmbeddeds = () => {
   $(embeddeds).each((index, embedded) => {
     $(embedded).attr('id', `embedded-${index}`);
     initEmbeddedBody(embedded);
+    initEmbeddedHeader(embedded);
   });
   return embeddeds;
 };
 
 export default bindEmbeddeds;
+export { isEmbedded, hasEmbeddedHeader, hasEmbeddedBody };
+
--- assets/scripts/embeddedHeader.js
+++ assets/scripts/embeddedHeader.js
@@ -1,32 +1,58 @@
-import { defaultHomepage } from './embeddedBody';
-import { embeddedSubmit } from './embeddedBody';
-
-
-// const embedded = $('.embedded-control')[0];
-// const embeddedHeader = $(embedded).find('.card-header')[0];
-// const embeddedBody = $(embedded).find('.card-body')[0];
-// const entrance = $(embedded).find('.entrance')[0];
-
-
-const bindHomePageButtons = (embedded) => {
-  if (!embedded || !$(embedded).hasClass('embedded-control')) return false;
-  const entrance = $(embedded).find('.entrance')[0];
-  if (!entrance) return false;
-  const homePageButtons = $(embedded).find('button[name=homepage]');
-  if (!homePageButtons) return false;
-  const homepage = $(entrance).find('div[name=homepage]')[0] || defaultHomepage();
-  homePageButtons.each((index, item) => {
-    $(item).on('click', () => {
-      $(entrance).html(homepage);
+const bindRefreshButton = (embedded) => {
+  if (!embedded || !$(embedded).hasClass('embedded-control')) throw new Error('参数元素应该是有标记class="embedded-control"');
+  const embeddedBody = $(embedded).find('.card-body')[0];
+  if (!embeddedBody) throw new Error('缺少card-body');
+  const iframeHolder = $(embeddedBody).find('.iframe-holder')[0];
+  if (!iframeHolder) throw new Error('缺少iframe-holder');
+  const iframe = $(iframeHolder).find('iframe')[0];
+  if (!iframe) throw new Error('缺少iframe');
+  const buttons = $(embedded).find('button[name=refresh]');
+  if (!buttons) return false;
+  buttons.each((index, button) => {
+    const icon = $(button).find('i')[0];
+    if (!icon || !$(icon).hasClass('fa-refresh')) throw new Error('没有refresh图标');
+    $(button).on('click', () => {
+      const src = $(iframe).attr('src');
+      if (src) {
+        if (!$(icon).hasClass('fa-pulse')) {
+          $(icon).addClass('fa-pulse');
+        }
+        $(iframe).on('load', () => {
+          $(icon).removeClass('fa-pulse');
+        });
+        $(iframe).attr('src', src);
+      }
     });
   });
   return embedded;
 };
 
-const bindEmbeddedHeader = (embedded) => {
-  if (!embedded || !$(embedded).hasClass('embedded-control')) return false;
-  bindHomePageButtons(embedded);
+const bindHomePageButton = (embedded) => {
+  if (!embedded || !$(embedded).hasClass('embedded-control')) throw new Error('bindHomePageButton的参数元素应该是有标记class="embedded-control"');
+  const embeddedBody = $(embedded).find('.card-body')[0];
+  if (!embeddedBody) throw new Error('缺少card-body');
+  const homepage = $(embeddedBody).find('.homepage')[0];
+  if (!homepage) throw new Error('缺少homepage');
+  const iframeHolder = $(embeddedBody).find('.iframe-holder')[0];
+  if (!iframeHolder) throw new Error('缺少iframe-holder');
+  const buttons = $(embedded).find('button[name=homepage]');
+  if (!buttons) return false;
+  buttons.each((index, button) => {
+    $(button).on('click', () => {
+      if (!$(iframeHolder).hasClass('hide')) { $(iframeHolder).addClass('hide'); }
+      $(homepage).removeClass('hide');
+    });
+  });
+  return embedded;
+};
+
+const initEmbeddedHeader = (embedded) => {
+  if (!embedded || !$(embedded).hasClass('embedded-control')) throw new Error('initEmbeddedHeader的参数元素应该是有标记class="embedded-control"');
+  const embeddedHeader = $(embedded).find('card-header');
+  if (!embeddedHeader) throw new Error('<div class="embedded-control">需要一个存在子元素<div class="card-header">');
+  bindHomePageButton(embedded);
+  bindRefreshButton(embedded);
   return true;
 };
 
-export { bindEmbeddedHeader };
+export default initEmbeddedHeader;
